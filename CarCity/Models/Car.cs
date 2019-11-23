378a6a98a1b61bda02372f3c3fd341fbf2aac273
zhanat1999 <47406837+zhanat1999@users.noreply.github.com>
2019-11-23 13:52:42 +0600

    first commit

---
+++ CarCity/Models/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarCity.Models
+{
+    public class Car {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string name { get; set; }
+        public Year year { get; set; }
+        public Types type { get; set; }
+        public City city { get; set; }
+        public IList<CarOwner> CarOwners { get; set; }
+    }
+}
